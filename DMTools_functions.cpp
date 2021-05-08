abe36044e05ea601b5d4267dfd945e8482839eed
Manuel Aragon <aragon.manuel@live.com>
2021-05-08 14:58:31 -0500

    Split functions file into cpp and header file

--- DMTools_functions.cpp
+++ DMTools_functions.cpp
@@ -1,11 +1,8 @@
 //
 //Contains Functions to be used throughout DMTools Project
 //
-#pragma once
-#include <string>
-#include <iostream>
-#include <sstream>
 
+#include "DMTools_functions.h"
 
 //convert from ascii to Hex
 std::string asciiToHex(std::string asciiString)
---
+++ DMTools_functions.h
@@ -0,0 +1,15 @@
+//
+//Contains Functions to be used throughout DMTools Project
+//
+
+#pragma once
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+//convert from ascii to Hex
+std::string asciiToHex(std::string);
+
+//convert from Hex to ascii
+std::string hexToAscii(std::string);
