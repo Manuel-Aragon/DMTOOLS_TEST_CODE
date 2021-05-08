aa550ee9a56a6aa3f44b7dfe91070ce035fca8fc
Manuel Aragon <aragon.manuel@live.com>
2021-05-08 14:41:59 -0500

    Added hex and string converter test file for profile saving

---
+++ asciiHex_converter.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+std::string asciiToHex(std::string asciiString)
+{
+  std::string hexString;
+  std::stringstream toHex_stream;
+  for (const auto &item : asciiString){
+    toHex_stream << std::hex << int(item);
+  }
+  hexString = toHex_stream.str();
+  return hexString;
+}
+
+std::string hexToAscii(std::string hexString)
+{
+  std::string asciiString;
+  std::stringstream toAscii_stream;    
+  for (size_t i = 0; i < hexString.length(); i += 2)
+    {
+        std::string part = hexString.substr(i, 2);
+        char ch = stoul(part, nullptr, 16);
+        asciiString += ch;
+    }
+  return asciiString;
+}
+
+int main()
+{
+  std::string test = "TEST NAME MOMDOASMDKL:SADJASKLDJASKLD:SA KLASDJKLASDJASLK:D  SKALJDLK:ASDJALKS:DJASL:KDJSAKD  ";
+  std::cout << test << std::endl;
+  std::cout << asciiToHex(test) << std::endl;
+  std::cout << hexToAscii(asciiToHex(test)) << std::endl;
+}
